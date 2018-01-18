7af749cd0279343d911bc460842fc166d9d38782
Guest User <Guest@epicodus-32.local>
2018-01-18 14:23:17 -0800

    Add decimalToBinary, and binaryToDecimal methods.

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,6 @@ function cryptosquare(inputString){
   }
   return outputArray[0][0];
 }
-
 function matrixCreate(rows, columns){
   var outMatrix = [];
   for(var i =0; i <rows; i++){
@@ -29,4 +28,42 @@ function matrixCreate(rows, columns){
     }
   }
   return outMatrix;
+}
+function decimalToBinary(inputDecimal){
+  var quotient = (inputDecimal/2);
+  var remainder = quotient*2 - Math.floor(quotient)*2;
+  var binaryOutput = "";
+  binaryOutput += remainder;
+  quotient = Math.floor(quotient);
+  while(quotient > 0 ){
+    quotient = quotient/2;
+    remainder = quotient*2 - Math.floor(quotient)*2;
+    quotient = Math.floor(quotient);
+    binaryOutput = remainder + binaryOutput;
+  }
+  return binaryOutput.toString();
+}
+function binaryToDecimal(inputBinary){
+  var arrLength = inputBinary.length;
+  var outputSum = 0;
+  var tracker = 0;
+  for(var i = arrLength-1; i >= 0; i--){
+    console.log(inputBinary[i] + ", " + tracker);
+    if(inputBinary[i]==1){
+      outputSum += Math.pow(2,tracker);
+    } else {
+
+    }
+    tracker++;
+  }
+  return outputSum;
+}
+function decimalToHexidecimal(inputDecimal){
+
+}
+
+function tester(){
+  for(var i = 0; i <= 100; i++){
+    console.log("Decimal: " + i + " Binary Equiv: " + decimalToBinary(i));
+  }
 }
